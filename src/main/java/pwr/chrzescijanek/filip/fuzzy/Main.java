0f77707ad97ecdb14ef0588e7f60d9a4740a55b3
Filip Chrześcijanek <filip702@gmail.com>
2019-03-01 21:33:32 +0100

    CLI parsing available now

--- src/main/java/pwr/chrzescijanek/filip/fuzzy/Main.java
+++ src/main/java/pwr/chrzescijanek/filip/fuzzy/Main.java
@@ -2,6 +2,14 @@ package pwr.chrzescijanek.filip.fuzzy;
 
 import java.io.IOException;
 import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import pwr.chrzescijanek.filip.fuzzyclassifier.Classifier;
 import pwr.chrzescijanek.filip.fuzzyclassifier.data.test.TestDataSet;
 import pwr.chrzescijanek.filip.fuzzyclassifier.data.test.TestRecord;
@@ -19,11 +27,25 @@ public class Main {
 	 * @throws IOException
 	 */
 	public static void main(final String... args) throws IOException {
-		ModelDto model = Utils.loadModel("model-path");
-		Classifier classifier = Utils.getClassifier(model);
-		List<TestRecord> records = Utils.readCsv("csv-path", model.getAttributes());
-		classifier.test(new TestDataSet(model.getAttributes(), records));	
-		records.forEach(System.out::println);
+		CommandLineParser parser = new DefaultParser();
+
+		Options options = new Options();
+		options.addRequiredOption( "d", "data", true, "path to CSV file with test data" );
+		options.addRequiredOption( "m", "model", true, "path to FUZZYMODEL file with fuzzy logic model data" );
+
+		try {
+		    CommandLine line = parser.parse( options, args );
+			
+			ModelDto model = Utils.loadModel(line.getOptionValue("model"));
+			Classifier classifier = Utils.getClassifier(model);
+			List<TestRecord> records = Utils.readCsv(line.getOptionValue("data"), model.getAttributes());
+			classifier.test(new TestDataSet(model.getAttributes(), records));	
+			records.forEach(System.out::println);
+		} catch( ParseException exp ) {
+		    System.out.println( "Exception: " + exp.getMessage() );
+		    HelpFormatter formatter = new HelpFormatter();
+		    formatter.printHelp("fuzzy-inference", options);
+		}
 	}
 
 }
